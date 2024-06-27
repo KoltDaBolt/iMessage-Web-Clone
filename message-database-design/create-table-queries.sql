d89632a59c0df261e6efd8ebb24c981f9358588e
Kolton Harper <koltdabolt1@gmail.com>
2024-06-27 18:55:09 -0500

    Reworking tables

--- message-database-design/create-table-queries.sql
+++ message-database-design/create-table-queries.sql
@@ -14,34 +14,43 @@ CREATE TABLE users (
     registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE user_keys (
+CREATE TABLE keys (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
-    public_key TEXT,
-    private_key TEXT,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    user_id VARCHAR(50),
+    public_key BLOB,
+    private_key BLOB,
+    FOREIGN KEY (user_id) REFERENCES users(username)
 );
 
-CREATE TABLE user_contacts (
+CREATE TABLE contacts (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    source_id INT,
-    target_id INT,
+    source_id VARCHAR(50),
+    target_id VARCHAR(50),
     target_firstname VARCHAR(50),
     target_lastname VARCHAR(50),
-    FOREIGN KEY (source_id) REFERENCES users(id),
-    FOREIGN KEY (target_id) REFERENCES users(id),
+    FOREIGN KEY (source_id) REFERENCES users(username),
+    FOREIGN KEY (target_id) REFERENCES users(username),
     UNIQUE (source_id, target_id)
 );
 
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT,
-    recipient_id INT,
-    message TEXT,
+    sender_id VARCHAR(50),
+    recipient_id VARCHAR(50),
+    message TEXT DEFAULT NULL,
+    status ENUM('SENT', 'DELIVERED', 'READ') DEFAULT 'SENT',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (recipient_id) REFERENCES users(id)
+    FOREIGN KEY (sender_id) REFERENCES users(username),
+    FOREIGN KEY (recipient_id) REFERENCES users(username)
+);
+
+CREATE TABLE attachments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    message_id INT,
+    attachment_type ENUM('IMAGE', 'VIDEO'),
+    attachment BLOB,
+    FOREIGN KEY (message_id) REFERENCES messages(id)
 );
 
 CREATE TABLE groups (
@@ -52,20 +61,20 @@ CREATE TABLE groups (
 CREATE TABLE group_members (
     id INT AUTO_INCREMENT PRIMARY KEY,
     group_id INT,
-    user_id INT,
+    user_id VARCHAR(50),
     FOREIGN KEY (group_id) REFERENCES groups(id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (user_id) REFERENCES users(username),
     UNIQUE (group_id, user_id)
 );
 
 CREATE TABLE group_messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    sender_id INT,
+    sender_id VARCHAR(50),
     group_id INT,
     message TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
+    FOREIGN KEY (sender_id) REFERENCES users(username),
     FOREIGN KEY (group_id) REFERENCES groups(id)
 );
 
@@ -77,12 +86,12 @@ CREATE TABLE emojis (
 
 CREATE TABLE reactions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id INT,
+    reactor_id VARCHAR(50),
     message_id INT,
     emoji_id INT,
-    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (reactor_id) REFERENCES users(username),
     FOREIGN KEY (message_id) REFERENCES messages(id),
     FOREIGN KEY (emoji_id) REFERENCES emojis(id),
-    UNIQUE (user_id, message_id, emoji_id)
+    UNIQUE (reactor_id, message_id, emoji_id)
 );
 
